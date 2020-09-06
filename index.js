eba6d6a64831997273a12703b04339fa92a86e24
vaishali <ambivaishali1998@gmail.com>
2020-09-06 13:28:29 +0530

    add all files

---
+++ index.js
@@ -0,0 +1,36 @@
+let count = 0;
+let n1 = 0;
+let i = 0;
+
+const print = () => {
+  let div = document.createElement("div");
+  div.id = i + " sticky";
+  document.getElementById("sticky").appendChild(div);
+  document.getElementById(i.toString() + " sticky").innerHTML =
+    "Sticky Keys Activated";
+  i++;
+};
+
+const stickykeys = () => {
+  let x = event.key;
+  if (x === "Shift") {
+    count++;
+    if (count === 1) {
+      n1 = Date.now();
+    }
+    let n2 = Date.now();
+    if (count === 5 && n2 - n1 < 5000) {
+      print();
+      console.log("sticky keys activated");
+      document
+        .getElementById("gif")
+        .setAttribute(
+          "src",
+          "https://media.giphy.com/media/TJO5x5QQM72Q0weWXN/giphy.gif"
+        );
+    }
+    count %= 5;
+  }
+};
+
+document.addEventListener("keydown", () => stickykeys());
